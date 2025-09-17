fcaad21697e57191133eff6dd7c9c7560942bf09
Aisha <aisha.siddiqua77@gmail.com>
2025-09-18 00:46:10 +0530

    error handling

--- services/DeliveryScheduler.js
+++ services/DeliveryScheduler.js
@@ -1,9 +1,29 @@
 class DeliveryScheduler {
   constructor(vehicles) {
+    vehicles.forEach((v, i) => {
+      if (typeof v.speed !== "number" || v.speed <= 0) {
+        throw new Error(`Vehicle[${i}] must have a valid positive speed.`);
+      }
+      if (typeof v.maxWeight !== "number" || v.maxWeight <= 0) {
+        throw new Error(`Vehicle[${i}] must have a valid positive maxWeight.`);
+      }
+      if (typeof v.availableAt !== "number" || v.availableAt < 0) {
+        v.availableAt = 0; // default
+      }
+    });
+
     this.vehicles = vehicles;
   }
 
   schedule(packages) {
+    packages.forEach((p, i) => {
+      if (typeof p.weight !== "number" || p.weight <= 0) {
+        throw new Error(`Package[${i}] must have a valid positive weight.`);
+      }
+      if (typeof p.distance !== "number" || p.distance <= 0) {
+        throw new Error(`Package[${i}] must have a valid positive distance.`);
+      }
+    });
     let pending = [...packages];
 
     // Utility: get all subsets under maxWeight
